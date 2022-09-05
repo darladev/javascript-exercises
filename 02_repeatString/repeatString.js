1313ce4284b8f76ab9026e19bacaff205439ff9c
Darla Dev <85639470+darladev@users.noreply.github.com>
2022-09-05 10:34:34 -0300

    Solved exercises 1 and 2. First draft.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(text, times) {
+    let fullText = text;
+    let result;
 
+    if (times < 0) {result = ("ERROR")}
+        else if (times === 0) {result = ""}
+        else{
+            for (let i = 1; i < times; i++) {
+                fullText += `${text}`;
+            }
+            result = `${fullText}`;
+            };
+            
+    return result;
 };
 
 // Do not edit below this line
