0c0f982d41f4e910520ae51c882262ea85eaacbf
Darla Dev <85639470+darladev@users.noreply.github.com>
2022-09-05 10:35:05 -0300

    Solved exercise 3. First draft.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(entry) {
+    const initialText = Array.from(entry);
+    let finalText = [];
+    let chars = initialText.length;
 
+    for (let i = 0; i < chars; i++) {
+        character = initialText.pop();
+        finalText.push(character);
+    }
+
+    return finalText.join("");
 };
 
 // Do not edit below this line
