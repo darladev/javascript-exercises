bfc96870012ce12f589bf88f099aca69c09da8fc
Darla Dev <85639470+darladev@users.noreply.github.com>
2022-10-01 08:36:45 -0300

    Done exercise 04! Remove from array. Piece of cake after a short break.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(theArgsData, ...theArgsFind) {
 
+    for (const arg of theArgsFind){
+        if (
+            theArgsData.indexOf(arg) > -1 && 
+            arg === theArgsData[theArgsData.indexOf(arg)]){
+
+            theArgsData.splice(theArgsData.indexOf(arg), 1);
+        }
+        else{continue}
+    }    
+    return theArgsData;
 };
 
 // Do not edit below this line
