2bac8d251b3f82315e4b8cfbcb70b0e2385128a6
Darla Dev <85639470+darladev@users.noreply.github.com>
2022-10-04 13:48:08 -0300

    Solved exercise 6 Leap Years. Pretty good I think.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4 === 0 && //every 4 years condition
+        year % 100 != 0 || //divisible by 100 are not leap years condition
+        year % 400 === 0) {//unless they are divisible by 400 condition
+            return true}
+    else {return false}
 };
 
 // Do not edit below this line
